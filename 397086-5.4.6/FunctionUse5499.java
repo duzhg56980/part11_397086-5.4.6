ec43cbdf552ff217d13ad5cfe94821190c1b4afe
duzhg56980 <guokey1996@gmail.com>
2024-02-19 11:03:04 +0800

    init commit

---
+++ 397086-5.4.6/FunctionUse5499.java
@@ -0,0 +1,6 @@
+import cn.hutool.core.util.PhoneUtil;
+public class FunctionUse5499 {
+public void funcUse() {
+PhoneUtil phoneutil = new PhoneUtil();
+}
+}
---
+++ 397086-5.4.6/FunctionUse5900.java
@@ -0,0 +1,6 @@
+import cn.hutool.core.util.StrUtil;
+public class FunctionUse5900 {
+public void funcUse() {
+StrUtil strutil = new StrUtil();
+}
+}
